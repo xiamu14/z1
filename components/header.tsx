bdf56e1d61376dbd02ed5ebed1455e2afc3270fd
ben <x_z_gfree@163.com>
2025-06-23 07:30:40 +0800

    feat: add some change

--- components/header.tsx
+++ components/header.tsx
@@ -8,16 +8,16 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <div className='fixed left-[50vw] top-[40px] w-[800px] translate-x-[-400px] rounded-full border-b border-stroke-soft-200 bg-white px-[10px] py-[6px]'>
-      <header className='mx-auto flex h-14 max-w-5xl items-center justify-between px-5'>
+      <header className='mx-auto flex h-14 max-w-5xl items-center justify-between px-[10px]'>
         <Link
           href='/'
           className='flex items-center gap-2 text-label-md text-text-strong-950'
         >
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
-            src='/images/new-logo.svg'
+            src='/images/logo.png'
             alt=''
-            className='size-9 h-[30px] w-[30px] object-contain'
+            className='size-9 h-[50px] w-[50px] object-contain'
           />
         </Link>
 
