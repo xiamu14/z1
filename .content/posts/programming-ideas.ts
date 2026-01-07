b3c423838c543e41970766cd95496b41dcf68b11
xiazhigang <xiazhigang@yev.com>
2026-01-07 10:43:00 +0800

    feat: update new post

---
+++ .content/posts/programming-ideas.ts
@@ -0,0 +1,14 @@
+export default {
+  "filePath": "/Users/ben/Documents/workspace/project/z1/posts/programming-ideas.md",
+  "content": "<h3>编程幻想</h3>\n<h4>Dashboard schema (? Admin)</h4>\n<ul>\n<li>基于文件的路由（Tanstack route）</li>\n<li>Context 全局变量共享</li>\n<li>Component -> Organization -> Layout -> Page 全员 Schema 配置</li>\n<li>Less Code</li>\n</ul>\n<h4>di-fetch</h4>\n<ul>\n<li>\n<p>完善 plugin 功能</p>\n<ul>\n<li>RN 支持的 client 封装</li>\n<li>支持 msw.js</li>\n</ul>\n</li>\n<li>\n<p>Hooks 灵活配置，支持路由 Pattern 来控制路由是否经过这些 hook（Less Code）</p>\n</li>\n</ul>",
+  "fileName": "programming-ideas",
+  "subject": [],
+  "md5": "462622d497097ad8ba3406a543627881",
+  "id": "CIkRvR",
+  "frontMatter": {
+    "title": "编程幻想",
+    "description": "偶尔出现的编程幻想",
+    "cover": "https://images.unsplash.com/photo-1677442136019-21780ecad995?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
+  "updateAt": 1767753692071
+}
---
+++ posts/programming-ideas.md
@@ -0,0 +1,23 @@
+---
+title: 编程幻想
+description: 偶尔出现的编程幻想
+cover: https://images.unsplash.com/photo-1677442136019-21780ecad995?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
+---
+
+### 编程幻想
+
+#### Dashboard schema (? Admin)
+
+- 基于文件的路由（Tanstack route）
+- Context 全局变量共享
+- Component -> Organization -> Layout -> Page 全员 Schema 配置
+- Less Code
+
+#### di-fetch
+
+- 完善 plugin 功能
+
+  - RN 支持的 client 封装
+  - 支持 msw.js
+
+- Hooks 灵活配置，支持路由 Pattern 来控制路由是否经过这些 hook（Less Code）
