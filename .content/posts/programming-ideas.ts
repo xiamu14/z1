2c5b43cab3032e1c6633f5962d97edf4f4cf5c46
ben <x_z_gfree@163.com>
2026-01-07 20:48:06 +0800

    feat: update programming ideas post and history

--- .content/posts/programming-ideas.ts
+++ .content/posts/programming-ideas.ts
@@ -1,14 +1,14 @@
 export default {
   "filePath": "/Users/ben/Documents/workspace/project/z1/posts/programming-ideas.md",
-  "content": "<h3>编程幻想</h3>\n<h4>Dashboard schema (? Admin)</h4>\n<ul>\n<li>基于文件的路由（Tanstack route）</li>\n<li>Context 全局变量共享</li>\n<li>Component -> Organization -> Layout -> Page 全员 Schema 配置</li>\n<li>Less Code</li>\n</ul>\n<h4>di-fetch</h4>\n<ul>\n<li>\n<p>完善 plugin 功能</p>\n<ul>\n<li>RN 支持的 client 封装</li>\n<li>支持 msw.js</li>\n</ul>\n</li>\n<li>\n<p>Hooks 灵活配置，支持路由 Pattern 来控制路由是否经过这些 hook（Less Code）</p>\n</li>\n</ul>",
+  "content": "<h3>代码构思</h3>\n<h4>Dashboard schema (? Admin)</h4>\n<ul>\n<li>基于文件的路由（Tanstack route）</li>\n<li>Context 全局变量共享</li>\n<li>Component -> Organization -> Layout -> Page 全员 Schema 配置</li>\n<li>Less Code</li>\n</ul>\n<h4>di-fetch</h4>\n<ul>\n<li>\n<p>完善 plugin 功能</p>\n<ul>\n<li>RN 支持的 client 封装</li>\n<li>支持 msw.js</li>\n</ul>\n</li>\n<li>\n<p>Hooks 灵活配置，支持路由 Pattern 来控制路由是否经过这些 hook（Less Code）</p>\n</li>\n</ul>",
   "fileName": "programming-ideas",
   "subject": [],
-  "md5": "462622d497097ad8ba3406a543627881",
-  "id": "CIkRvR",
+  "md5": "541f8428d27db5e58f04aacd9d6aabdf",
+  "id": "DHL5oL",
   "frontMatter": {
-    "title": "编程幻想",
-    "description": "偶尔出现的编程幻想",
+    "title": "代码构思",
+    "description": "灵感乍现的代码构思",
     "cover": "https://images.unsplash.com/photo-1677442136019-21780ecad995?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
-  "updateAt": 1767753692071
+  "updateAt": 1767790068534
 }
--- posts/programming-ideas.md
+++ posts/programming-ideas.md
@@ -1,10 +1,10 @@
 ---
-title: 编程幻想
-description: 偶尔出现的编程幻想
+title: 代码构思
+description: 灵感乍现的代码构思
 cover: https://images.unsplash.com/photo-1677442136019-21780ecad995?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
 ---
 
-### 编程幻想
+### 代码构思
 
 #### Dashboard schema (? Admin)
 
